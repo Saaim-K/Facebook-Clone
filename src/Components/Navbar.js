dfbf5aed72761858e97fb325069c23c1cf531ef7
Saaim-K <saaimkhan221@gmail.com>
2022-09-14 03:23:18 +0500

    Facebook-Clone

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <div className='logo'></div>
+            <div className='navabr'></div>
+            <div className='profile'></div>
+        </div>
+    )
+}
