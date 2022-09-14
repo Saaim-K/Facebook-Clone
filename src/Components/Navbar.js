d18f8cb162a514f5f758a83f8a248f7e093233d0
Saaim-K <saaimkhan221@gmail.com>
2022-09-14 19:43:45 +0500

    Facebook-Clone

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,15 +4,14 @@ export default function Navbar(props) {
     return (
         <>
             <div className="titleBar">
-                <div>
-                    <div className='logo'>
-                        <div className="fb-logo"><img className='fblogo' src={props.logo} alt="facebook-logo" /></div>
-                        <div className="fb-search"><input type="text" name="search" placeholder='Search Facebook' /></div>
-                    </div>
-
-                    <div className='navabr'>{props.search}</div>
-                    <div className='profile'>{props.profile}</div>
+                <div className='logo'>
+                    <div className="fb-logo"><img className='fblogo' src={props.logo} alt="facebook-logo" /></div>
+                    <div className="fb-search"><input type="text" name="search" placeholder='Search Facebook' autoComplete='off' /></div>
                 </div>
+                <div className="home">
+                    <div className='navabr'>{props.search}</div>
+                </div>
+                <div className='profile'>{props.profile}</div>
 
 
 
