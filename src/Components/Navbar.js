50d71c59d711fb43b05235c8e25f8d169e1a0c8a
Saaim-K <saaimkhan221@gmail.com>
2022-09-16 19:51:34 +0500

    Facebook-Clone

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { AiFillHome } from "react-icons/ai";
+import { FaPlay, FaUserCircle } from "react-icons/fa";
+import { HiUserGroup } from "react-icons/hi";
+import { SiFacebookgaming } from "react-icons/si";
+import { BsFillGrid3X3GapFill, BsMessenger } from "react-icons/bs";
+import { MdNotificationsActive } from "react-icons/md";
 
 export default function Navbar(props) {
     return (
@@ -9,10 +14,19 @@ export default function Navbar(props) {
                     <div className="fb-logo"><img className='fblogo' src={props.logo} alt="facebook-logo" /></div>
                     <div className="fb-search"><input type="text" name="search" placeholder='Search Facebook' autoComplete='off' /></div>
                 </div>
-                <div className="home">
-                    <div className='navbar'><AiFillHome /></div>
+                <div className="tab">
+                    <div className='home'><AiFillHome /></div>
+                    <div className='home'><FaPlay /></div>
+                    <div className='home'><HiUserGroup /></div>
+                    <div className='home'><SiFacebookgaming /></div>
                 </div>
-                <div className='profile'>{props.profile}</div>
+                <div className='profile'>
+                    <div className="profile"><BsFillGrid3X3GapFill /></div>
+                    <div className="profile"><BsMessenger /></div>
+                    <div className="profile"><MdNotificationsActive /></div>
+                    <div className="profile"><FaUserCircle /></div>
+
+                </div>
 
 
 
