4ca266d61c1d8c96875fa51865f1324717330e26
Saaim-K <saaimkhan221@gmail.com>
2022-09-21 01:29:10 +0500

    Fakebook

--- my-fakebook/src/Compnents/Navbar/Navbar.js
+++ my-fakebook/src/Compnents/Navbar/Navbar.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import './Navbar.css'
 
-export default function Navbar() {
+function Navbar() {
   return (
     <>
-        
+      <div className='fb-search'>
+        <span className='fb-logo'><img src="https://img.icons8.com/fluency/50/000000/facebook-new.png" /></span>
+        <span className='fb-input'></span>
+      </div>
+
+      <div className='fb-home'>
+        <span className="fb-"></span>
+        <span className="fb-"></span>
+        <span className="fb-"></span>
+        <span className="fb-"></span>
+        <span className="fb-"></span>
+
+      </div>
+
+      <div className='fb-account'>
+        <span className="fb-acc"></span>
+        <span className="fb-acc"></span>
+        <span className="fb-acc"></span>
+        <span className="fb-acc"></span>
+      </div>
+
     </>
   )
 }
+export default Navbar
