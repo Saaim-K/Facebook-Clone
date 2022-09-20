30134b3c795dbbdbc1baf40c1f04ab18d6a9954b
Saaim-K <saaimkhan221@gmail.com>
2022-09-21 03:11:59 +0500

    Fakebook

--- my-fakebook/src/Compnents/Navbar/Navbar.js
+++ my-fakebook/src/Compnents/Navbar/Navbar.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import './Navbar.css'
+import { MdSearch, MdOutlineOndemandVideo } from "react-icons/md";
+import { FaHome  } from "react-icons/fa";
+import { HiUserGroup } from "react-icons/hi";
+import { SiFacebookgaming } from "react-icons/si";
+import { BsFillGrid3X3GapFill, BsMessenger } from "react-icons/bs";
+
+
+
+
+
+
 
 function Navbar() {
   return (
@@ -7,24 +18,24 @@ function Navbar() {
       <div className='fb-search'>
         <span className='fb-logo'><img src="https://img.icons8.com/fluency/50/000000/facebook-new.png " alt='facebook-logo' /></span>
         <span className='fb-input'>
-          <span className='fb-input-logo'></span>
+          <span className='fb-input-logo'><MdSearch /></span>
           <span className='fb-input-search'></span>
         </span>
       </div>
 
       <div className='fb-home'>
+        <span className="fb-"><FaHome /></span>
+        <span className="fb-"><MdOutlineOndemandVideo /></span>
         <span className="fb-"></span>
-        <span className="fb-"></span>
-        <span className="fb-"></span>
-        <span className="fb-"></span>
-        <span className="fb-"></span>
+        <span className="fb-"><HiUserGroup /></span>
+        <span className="fb-"><SiFacebookgaming /></span>
 
       </div>
 
       <div className='fb-account'>
-        <span className="fb-acc"></span>
-        <span className="fb-acc"></span>
-        <span className="fb-acc"></span>
+        <span className="fb-acc"><BsFillGrid3X3GapFill /></span>
+        <span className="fb-acc"><BsMessenger /></span>
+        {/* <span className="fb-acc"><HiOutlineBellAlert /></span> */}
         <span className="fb-acc"></span>
       </div>
 
