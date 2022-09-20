1784e51962adea0e7372edd462e24fd3cd1b5467
Saaim-K <saaimkhan221@gmail.com>
2022-09-21 02:42:07 +0500

    Fakebook

--- my-fakebook/src/Compnents/Navbar/Navbar.js
+++ my-fakebook/src/Compnents/Navbar/Navbar.js
@@ -3,10 +3,13 @@ import './Navbar.css'
 
 function Navbar() {
   return (
-    <>
+    <div className='navbar'>
       <div className='fb-search'>
-        <span className='fb-logo'><img src="https://img.icons8.com/fluency/50/000000/facebook-new.png" /></span>
-        <span className='fb-input'></span>
+        <span className='fb-logo'><img src="https://img.icons8.com/fluency/50/000000/facebook-new.png " alt='facebook-logo' /></span>
+        <span className='fb-input'>
+          <span className='fb-input-logo'></span>
+          <span className='fb-input-search'></span>
+        </span>
       </div>
 
       <div className='fb-home'>
@@ -25,7 +28,7 @@ function Navbar() {
         <span className="fb-acc"></span>
       </div>
 
-    </>
+    </div>
   )
 }
 export default Navbar
