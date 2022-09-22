e66365a8f404d3aaec0039ba32f1c5c816d0a46c
Saaim-K <saaimkhan221@gmail.com>
2022-09-23 04:24:56 +0500

    Fakebook

--- my-fakebook/src/Compnents/Left-Sidebar/index.js
+++ my-fakebook/src/Compnents/Left-Sidebar/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import './index.css'
+import '.'
 
 export default function LeftSidebar() {
     return (
 
         <div className="sidebar">
-            <div><img src="" alt="" /></div>
+            <div><img src="" alt="ccc" /></div>
         </div>
 
 
