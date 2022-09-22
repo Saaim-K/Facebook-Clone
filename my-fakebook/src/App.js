5bd77e5172ce5945ebfab32958ef56377852d34e
Saaim-K <saaimkhan221@gmail.com>
2022-09-23 04:22:11 +0500

    Fakebook

--- my-fakebook/src/App.js
+++ my-fakebook/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './Compnents/Navbar';
 import Body from './Compnents/Body';
+import LeftSidebar from './Compnents/Left-Sidebar';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Body />
+      {/* <Body /> */}
+      <LeftSidebar />
     </div>
   );
 }
---
+++ my-fakebook/src/Compnents/Left-Sidebar/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+export default function LeftSidebar() {
+    return (
+
+        <div className="sidebar">
+            <div><img src="" alt="" /></div>
+        </div>
+
+
+    )
+}
